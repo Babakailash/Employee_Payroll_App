429c11c4c2f35b4baa51dd69090e4ca0ee8c3ddf
Babakailash <nathkailash2020@gmail.com>
2021-06-15 02:08:57 +0530

    Added:addEventListener for Name and Salary Validation

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -13,4 +13,11 @@ window.addEventListener('DOMContentLoaded', (event)=> {
             textError.textContent=e;
         }
     });
+
+    const salary = document.querySelector('#salary');
+    const output = document.querySelector('.salary-output');
+    output.textContent=salary.value;
+    salary.addEventListener('input', function(){
+        output.textContent = salary.value;
+    });
 });
