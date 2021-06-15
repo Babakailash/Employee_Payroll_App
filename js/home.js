8d521dee934df41d786acaa4cc7efdd9d2a2acd6
Babakailash <nathkailash2020@gmail.com>
2021-06-16 03:15:02 +0530

    Added:Js File using Template Literals in Tabular Formate

---
+++ js/home.js
@@ -0,0 +1,32 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    createInnerHtml();
+});
+/*Template literal ES6 features*/
+const createInnerHtml = () => {
+    const innerHtml = `
+    <tr>
+        <th></th>
+        <th>Name</th>
+        <th>Gender</th>
+        <th>Department</th>
+        <th>Salary</th>
+        <th>Start Date</th>
+        <th>Actions</th>
+        </tr>
+        <tr><!--Hard Coding only test purpose -->
+        <td><img class="profile" alt="" src="../assets/profile-images/Ellipse -2.png">
+        </td>
+        <td>Kailashnath Vishwakarma</td>
+        <td>Male</td>
+        <td><div class="dept-label">Engineering</div>
+        <div class="dept-label">Finance</div></td>
+        <td>3000000</td>
+        <td> 1 Nov 2019</td>
+        <td>
+            <img id="1" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
+            <img id="1" alt="edit" onclick="update(this)" src="../assets/icons/create-black-18dp.svg">
+        </td>
+    </tr>
+    `;
+        document.querySelector('#table-display').innerHTML = innerHtml;
+}
